3ae86ea76e2b12e6ae024a083c781a4b4207cec3
laifujun <laifujun88@gmail.com>
2024-10-31 05:04:24 +0000

    home draft

--- www/home.php
+++ www/home.php
@@ -1,3 +1,8 @@
+<?php
+error_reporting(E_ALL);
+ini_set('display_errors', 1);
+?>
+
 <!DOCTYPE html>
 <html lang="en">
 <head>
@@ -10,7 +15,7 @@
 
         <!-- Search Bar -->
         <form method="GET" class="search-form">
-            <input type="text" name="search" placeholder="Search for books..." value="<?php echo htmlspecialchars($search); ?>">
+            <input type="text" name="search" placeholder="Search for books..." value="">
             <button type="submit">Search</button>
         </form>
 
@@ -21,44 +26,44 @@
                 <label for="author">Author:</label>
                 <select name="author">
                     <option value="">All Authors</option>
-                    <?php foreach ($authors as $author): ?>
+                    <!-- <?php foreach ($authors as $author): ?>
                         <option value="<?php echo $author['author']; ?>" <?php if ($selectedAuthor == $author['author']) echo 'selected'; ?>>
                             <?php echo $author['author']; ?>
                         </option>
-                    <?php endforeach; ?>
+                    <?php endforeach; ?> -->
                 </select>
 
                 <!-- Genre Filter -->
                 <label for="genre">Genre:</label>
                 <select name="genre">
                     <option value="">All Genres</option>
-                    <?php foreach ($genres as $genre): ?>
+                    <!-- <?php foreach ($genres as $genre): ?>
                         <option value="<?php echo $genre['genre']; ?>" <?php if ($selectedGenre == $genre['genre']) echo 'selected'; ?>>
                             <?php echo $genre['genre']; ?>
                         </option>
-                    <?php endforeach; ?>
+                    <?php endforeach; ?> -->
                 </select>
 
                 <!-- Language Filter -->
                 <label for="language">Language:</label>
                 <select name="language">
                     <option value="">All Languages</option>
-                    <?php foreach ($languages as $language): ?>
+                    <!-- <?php foreach ($languages as $language): ?>
                         <option value="<?php echo $language['language']; ?>" <?php if ($selectedLanguage == $language['language']) echo 'selected'; ?>>
                             <?php echo $language['language']; ?>
                         </option>
-                    <?php endforeach; ?>
+                    <?php endforeach; ?> -->
                 </select>
 
                 <!-- Type Filter -->
                 <label for="type">Type:</label>
                 <select name="type">
                     <option value="">All Types</option>
-                    <?php foreach ($types as $type): ?>
+                    <!-- <?php foreach ($types as $type): ?>
                         <option value="<?php echo strtolower($type); ?>" <?php if ($selectedType == strtolower($type)) echo 'selected'; ?>>
                             <?php echo $type; ?>
                         </option>
-                    <?php endforeach; ?>
+                    <?php endforeach; ?> -->
                 </select>
 
                 <button type="submit">Apply Filters</button>
